72c258bc264af9ba9d81fb6737f46ea0fd0ebe87
KENGkuy <ghyrgkirk@gmail.com>
2022-08-20 20:44:11 +0700

    อัปเดต

--- Cake shop program/Form1.vb
+++ Cake shop program/Form1.vb
@@ -6,71 +6,71 @@
         c = 130
         d = 140
         f = 150
-        If CheckBox1.Checked Then
-            total += a
+        'If CheckBox1.Checked Then
+        total += a
             count += 1
 
         End If
-        If CheckBox2.Checked Then
-            total += c
+        ' If CheckBox2.Checked Then
+        total += c
             count += 1
         End If
-        If CheckBox3.Checked Then
-            total += d
+        'If CheckBox3.Checked Then
+        total += d
             count += 1
         End If
-        If CheckBox4.Checked Then
-            total += f
+        'If CheckBox4.Checked Then
+        total += f
             count += 1
         End If
-        If CheckBox5.Checked Then
-            total += a
+        'If CheckBox5.Checked Then
+        total += a
             count += 1
         End If
-        If CheckBox6.Checked Then
-            total += b
+        'If CheckBox6.Checked Then
+        total += b
             count += 1
         End If
-        If CheckBox7.Checked Then
-            total += c
+        'If CheckBox7.Checked Then
+        total += c
             count += 1
         End If
-        If CheckBox8.Checked Then
-            total += d
+        'If CheckBox8.Checked Then
+        total += d
             count += 1
         End If
-        If CheckBox9.Checked Then
-            total += f
+        'If CheckBox9.Checked Then
+        total += f
             count += 1
         End If
-        If CheckBox10.Checked Then
-            total += a
+        'If CheckBox10.Checked Then
+        total += a
             count += 1
         End If
 
         TextBox1.Text = total
         Label2.Text = count
 
-        If RadioButton1.Checked = True Then
-            TextBox1.Text = Val(TextBox1.Text)
+        'If RadioButton1.Checked = True Then
+        TextBox1.Text = Val(TextBox1.Text)
         ElseIf RadioButton2.Checked = True Then
-            TextBox1.Text = Val(TextBox1.Text) + 50
+        'TextBox1.Text = Val(TextBox1.Text) + 50
         End If
 
         Dim g As Integer
         If Val(ListBox1.SelectedItems.Count) Then
             ListBox1.Text = g
         End If
-        TextBox2.Text = ListBox1.SelectedItem * Val(TextBox1.Text)
+        'TextBox2.Text = ListBox1.SelectedItem * Val(TextBox1.Text)
 
         DataGridView1.Rows(0).Cells(3).Value = Val(TextBox2.Text)
-        DataGridView1.Rows(0).Cells(2).Value = ListBox1.SelectedItems
+        ' DataGridView1.Rows(0).Cells(2).Value = ListBox1.SelectedItems
         If RadioButton1.Checked = True Then
-            DataGridView1.Rows(0).Cells(1).Value = "1/2ปอนด์"
+            ' DataGridView1.Rows(0).Cells(1).Value = "1/2ปอนด์"
         ElseIf RadioButton2.Checked = True Then
-            DataGridView1.Rows(0).Cells(1).Value = "1ปอนด์"
+            ' DataGridView1.Rows(0).Cells(1).Value = "1ปอนด์"
         End If
-        DataGridView1.Rows(0).Cells(0).Value = Val(Label2.Text)
+        'DataGridView1.Rows(0).Cells(0).Value = Val(Label2.Text)
 
     End Sub
 
